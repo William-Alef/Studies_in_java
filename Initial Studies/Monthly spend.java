a2d83be9c5723b41f1e3b1a74bd5b207baf8dd1c
Robot_Alef <69725099+William-Alef@users.noreply.github.com>
2020-09-07 13:57:31 -0300

    Create Monthly spend.java

---
+++ Initial Studies/Monthly spend.java
@@ -0,0 +1,18 @@
+class GastoTrimestral {
+    public static void main (final String[] args) {
+        int ja = 15000;
+            System.out.println(ja);
+        
+        int fev = 23000;
+            System.out.println(fev);
+        
+        int mar = 17000;
+            System.out.println(mar);
+
+        int SomaTrimestal = ja + fev + mar;
+            System.out.println(SomaTrimestal);
+
+        int MediaMensal = SomaTrimestal /3;
+            System.out.println(MediaMensal);
+    }
+}
