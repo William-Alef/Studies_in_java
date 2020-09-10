de30b5614465f1926e8e6748bbd3b94555e7f48c
Robot_Alef <69725099+William-Alef@users.noreply.github.com>
2020-09-10 01:24:01 -0300

    Update Calculator_v0.1.java

--- Initial Studies/Calculator_v0.1.java
+++ Initial Studies/Calculator_v0.1.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 	public class Calculator {
 
-		public static void main(String[] args) {
-		Scanner ler = new Scanner (System.in);
-		int a, b;
+	public static void main(String[] args) {
+	Scanner ler = new Scanner (System.in);
+	int a, b;
 
-			System.out.printf("Type a number: ");
-			a = ler.nextInt();
+		System.out.printf("Type a number: ");
+		a = ler.nextInt();
 
-			System.out.printf("Other number: ");
-			b = ler.nextInt();
+		System.out.printf("Other number: ");
+		b = ler.nextInt();
 
-			System.out.printf("\nResult: \n");
-			System.out.printf("\n%d + %d = %3d\n", a, b, (a + b));
-			System.out.printf("\n%d - %d = %3d\n", a, b, (a - b));
-			System.out.printf("\n%d * %d = %3d\n", a, b, (a * b));
-			System.out.printf("\n%d / %d = %3d (Division)\n", a ,b, (a / b));
-			System.out.printf("\n%d / %d = %6.2f (Rest of division)\n", a, b, ((double)a / b));
-		}			
-	}
+		System.out.printf("\nResult: \n");
+		System.out.printf("\n%d + %d = %3d\n", a, b, (a + b));
+		System.out.printf("\n%d - %d = %3d\n", a, b, (a - b));
+		System.out.printf("\n%d * %d = %3d\n", a, b, (a * b));
+		System.out.printf("\n%d / %d = %3d (Division)\n", a ,b, (a / b));
+		System.out.printf("\n%d / %d = %6.2f (Rest of division)\n", a, b, ((double)a / b));
+	}			
+}
