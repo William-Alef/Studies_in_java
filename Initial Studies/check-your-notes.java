f58ab4f84a9f7d07d42b9eef75e73f5142c0ac6f
Robot_Alef <69725099+William-Alef@users.noreply.github.com>
2020-09-14 18:30:11 -0300

    Add files via upload

---
+++ Initial Studies/check-your-notes.java
@@ -0,0 +1,30 @@
+import java.security.Principal;
+import java.util.Scanner;
+    
+class exer {
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+    
+        System.out.println("\tType your notes\t");
+        System.out.println(" ");
+        System.out.print("First note: ");
+            int nu1 = scan.nextInt();
+
+        System.out.print("Second note: ");
+            int nu2 = scan.nextInt();
+
+        System.out.print("Third note: ");
+            int nu3 = scan.nextInt();
+
+        System.out.print("Fourth note: ");
+            int nu4 = scan.nextInt();
+
+        System.out.println("");
+    
+            int averange = (nu1 + nu2 + nu3 + nu3) /4;
+        System.out.print("Total average: " + averange);
+
+
+    }   
+ }
