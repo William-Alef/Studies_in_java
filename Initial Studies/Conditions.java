74e67e297ff2ef14d6a3ebf758a8bf6e3ca5ff80
Robot_Alef <69725099+William-Alef@users.noreply.github.com>
2020-09-14 19:41:04 -0300

    Add files via upload

---
+++ Initial Studies/Conditions.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+    
+class have {
+    public static void main(String[] args) {
+
+        Scanner Scan = new Scanner(System.in);
+
+            System.out.println("What's de price: ");
+
+            Double price = Scan.nextDouble();
+
+            if (price <= 20){
+                System.out.println("Cool, veri nice price!");
+            }    
+            else if (price > 20 && price < 25){
+                System.out.println("Good!");
+            }
+            else if (price > 25 && price < 27){
+                System.out.println("Not bad!");
+            }
+            else {
+                System.out.println("SO BAD!");
+
+        }   
+    }   
+}
